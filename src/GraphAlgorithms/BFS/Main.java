6c955ae3ba8c99cc02d86011ca516937c3217285
Anuradha Sanjeewa Wickramarachchi <anuradhawick@gmail.com>
2015-12-22 12:08:33 +0530

    BFS implementation

--- src/GraphAlgorithms/BFS/Main.java
+++ src/GraphAlgorithms/BFS/Main.java
@@ -3,7 +3,7 @@ package GraphAlgorithms.BFS;
 /**
  * Created by Anuradha Sanjeewa on 22/12/2015.
  */
-public class BFS {
+public class Main {
     public static void main(String[] args) {
         System.out.println("asda");
     }
